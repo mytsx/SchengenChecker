0a1d38f2fa063b93de5d719ac64261b89f29decb
mytsx <41596212+mytsx@users.noreply.github.com>
2024-12-13 11:17:51 +0000

    replit deploy build ayarlamaları yapıldı

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 
 #!/bin/bash
-python3 SchengenChecker.py & python3 app.py
+python3 SchengenChecker.py &        # Checker scriptini arka planda çalıştır
+gunicorn -w 4 -b 0.0.0.0:8080 app:app  # Flask uygulamasını Gunicorn ile çalıştır
